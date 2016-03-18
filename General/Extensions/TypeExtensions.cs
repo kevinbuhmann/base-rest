4362e42ddd46299ef4a429c2b5cec77c138c5a8f
Kevin Phelps <KevinPhelps11@gmail.com>
2016-03-17 21:13:12 -0500

    move entension method to general

---
+++ General/Extensions/TypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace General.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static int CountInheritanceDepth(this Type type)
+        {
+            int count = 0;
+
+            Type baseType = type;
+            while (baseType != null)
+            {
+                count++;
+                baseType = baseType.BaseType;
+            }
+
+            return count;
+        }
+    }
+}
