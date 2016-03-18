b487b5a1afeb4a4279299cfb0574791cb85acbac
Kevin Phelps <KevinPhelps11@gmail.com>
2016-03-17 21:57:55 -0500

    build non profit tables and add to items table

---
+++ General/Extensions/ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace General.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T RandomItem<T>(this T[] array)
+        {
+            Random random = new Random((int)(DateTime.Now.Ticks % int.MaxValue));
+            return array[random.Next() % array.Length];
+        }
+    }
+}
